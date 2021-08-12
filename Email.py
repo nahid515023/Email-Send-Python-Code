7fab0c0c37d1e65c10654e8ff4ebf0d5dd2c50df
nahid515023 <60315276+nahid515023@users.noreply.github.com>
2021-08-12 23:01:35 +0600

    first_update

--- Email.py
+++ Email.py
@@ -1,35 +1,66 @@
 import smtplib
 from email.message import EmailMessage
 import imghdr
-
-sender = 'nahid@gmail.com'
-password = ''
-
-massage = EmailMessage()
-massage['Subject'] = "i love you nahid."
-massage['From'] = sender
-massage['To'] = 'nahid23@gmail.com'
-massage.set_content('some file ?')
-files = ['IMG.jpg','nahid.pdf','n.mp3']
-for file in files:
-    with open(file, "rb") as f:
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-        file_name = f.name
-    if(file_type == None):
-        massage.add_attachment(file_data, maintype='application',
-                               subtype='octet-stream', filename=file_name)
-
-    else:
-        massage.add_attachment(file_data, maintype='image',
-                               subtype=file_type, filename=file_name)
+import getpass
 
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-# with smtplib.SMTP('localhost', 1025) as smtp:
-# python -m smtpd -c DebuggingServer -n localhost:1025
-   # smtp.ehlo()
-   # smtp.starttls()
-   # smtp.ehlo()
-   smtp.login(sender,password)
-   smtp.send_message(massage)
+def main():
+    print("\t\t-------------------------LogIn Gmail---------------------------\n")
+    sender = input("\t\tEnter Your Gmail Address: ")
+    password = getpass.getpass("\t\tEnter Your Password: ")
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        try:
+            smtp.login(sender, password)
+            massage = EmailMessage()
+            massage['From'] = sender
+            massage['To'] = input("\t\tTo: ")
+            massage['Subject'] = input("\t\tSubject: ")
+            body = input("\t\tBody: ")
+            massage.set_content(body)
+
+            print("\t\tDo you want to add files(Y/N)?", end="")
+            y=input()
+            if(y=='Y' or y=='y'):
+                c = True
+                files = []
+                while(c):
+                    url = input("\t\tEnter File URL: ")
+                    files.append(url)
+                    print("\t\tDo you want to add more files(Y/N)?", end="")
+                    x = input()
+                    if(x == 'N'or x=='n'):
+                        c = False
+
+                for file in files:
+                    with open(file, "rb") as f:
+                        if f != None:
+                            file_data = f.read()
+                            file_type = imghdr.what(f.name)
+                            file_name = f.name
+                            if(file_type == None):
+                                massage.add_attachment(file_data, maintype='application',
+                                                    subtype='octet-stream', filename=file_name)
+
+                            else:
+                                massage.add_attachment(file_data, maintype='image',
+                                                    subtype=file_type, filename=file_name)
+            try:
+                smtp.send_message(massage)
+                print("\n\t\tEmail has been successfully delivered.")
+            except smtplib.SMTPSenderRefused:
+                print("\n\t\tEmail is not delivered!")
+                                                    
+        except smtplib.SMTPAuthenticationError:
+            print("\n\t\tYour email and password are wrong!")
+
+        # with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        #     # with smtplib.SMTP('localhost', 1025) as smtp:
+        #     # python -m smtpd -c DebuggingServer -n localhost:1025
+        #     # smtp.ehlo()
+        #     # smtp.starttls()
+        #     # smtp.ehlo()
+        #     smtp.login(sender, password)
+
+
+if __name__ == '__main__':
+    main()
